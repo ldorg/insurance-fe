354a65abff4022fb4d5afa433c65dd28d849ad47
Logan Donley <ldonley@cloudbees.com>
2021-10-14 10:01:00 -0400

    Updating chart to use the correct var

--- variableReplace.sh
+++ variableReplace.sh
@@ -3,6 +3,7 @@
 echo "Replacing variables"
 . /usr/share/nginx/html/.env
 if [[ -v FM_KEY ]]; then
+  echo "Replacing $VITE_FM_KEY with $FM_KEY"
   find /usr/share/nginx/html -type f -print0 | xargs -0 sed -i "s/$VITE_FM_KEY/$FM_KEY/g"
 fi
 if [[ -v BACKEND_URL ]]; then
